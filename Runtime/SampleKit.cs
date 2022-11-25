9a7a5f354d86f211a366885adc7c3b5a267e8dde
erlendbro01 <64799691+erlendbro01@users.noreply.github.com>
2022-11-25 15:25:37 +0100

    Testin01

---
+++ Runtime/SampleKit.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace erlend
+{ 
+public class SampleKit : MonoBehaviour
+{
+        public float test = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+}
